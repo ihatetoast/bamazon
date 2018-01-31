c2fb039496da45075d0a58d368fb8c7d606c1ae6
Katy <ihatetoast@gmail.com>
2018-01-31 15:55:23 -0600

    add stock done

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # KaBamazon: Katy's Bamazon
 A storefront on the CLI using Node and MySQL
 
+## CUSTOMER PORTAL:
+
 The Customer portal displays a table with the items, their ids, and the amount for each product.
 
-The customer chooses a product by entering the id and how much the of the item the customer wants. If the id is valid and there is enough stock, the customer gets a receipt for the order, which includes Texas sales tax and an option to shop again. 
+In the screencast below, the customer chooses a product by entering the id and how much the of the item the customer wants. If the id is valid and there is enough stock, the customer gets a receipt for the order, which includes Texas sales tax and an option to shop again. 
 
 LICEcap of customer portal:
-![screencast of a customer shopping](./bamazon_customer.gif)
+![screencast of customer](./bamazon_customer.gif)
 
+## MANAGER PORTAL:
+
+The manager has several options. 
+
+In the screencast below, the manager selects two options. The first one returns a table of all of the stock (ids, product names, quantity) in her store. Items that are below par are highlighted. 
+The second option returns just those items low in stock. 
+
+LICEcap of manager portal (view all stock & view low stock functions):
+![screencast of manager portal](./bamazon_manager_1.gif)
 
 Packages used:
 
--- bamazonManager.js
+++ bamazonManager.js
@@ -27,7 +27,7 @@ function managerView(){
       type: "list",
       name: "manager",
       message: "Choose from list of tasks:",
-      choices: ["View products", "View items low in stock", "Add to stock", "Add a new product to your stock"]
+      choices: ["View products", "View items low in stock", "Add to stock", "Add a new product to your stock", "Close program"]
     }
   ])
   .then((answers) => {
@@ -39,10 +39,13 @@ function managerView(){
         viewLowStock();
         break;
       case "Add to stock":
-        console.log("Add to stock");
+        addToStock()
         break;
       case "Add a new product to your stock":
-        console.log("Add a new product to your stock chosen");
+        addNewProduct()
+        break;
+      case "Close program":
+        connection.end()
         break;
       default:
         console.log("oops");
@@ -53,30 +56,10 @@ function viewProducts() {
   console.log(`${colors.rainbow("KABAMAZON")} Product View:
   Remember: Your bonus depends on moving inventory out!
   We are all about the bottom line here. Give up the family.`);
-    
-  connection.query("SELECT * FROM products", function(err, res){
-    //instatiate the table:
-    var table = new Table({
-      head: ['ID', 'Item', 'Price', 'Qty'], 
-      colWidths: [5, 40, 15, 10]
-    });
-
-    if (err) throw err;
-
-    for (var i = 0; i < res.length; i++) {  
-      let qty = res[i].stock_quantity;
-      if(qty < 5){
-        qty = colors.magenta(qty)
-        }
-      table.push(
-        [res[i].item_id, res[i].product_name, res[i].price, qty]
-        )
-      };
-      console.log(table.toString());
-      setTimeout(()=>{
-        managerView();
-      }, 1000);
-  })
+  showTable();
+  setTimeout(()=>{
+    managerView();
+  }, 1500);
 }
 
 function viewLowStock(){
@@ -89,9 +72,7 @@ function viewLowStock(){
       head: ['ID', 'Item', '< 5'], 
       colWidths: [5, 40, 10]
     });
-
     if (err) throw err;
-
     for (var i = 0; i < res.length; i++) {  
       let qty = res[i].stock_quantity;
       table.push(
@@ -101,10 +82,90 @@ function viewLowStock(){
       console.log(table.toString());
       setTimeout(()=>{
         managerView();
-      }, 1000);
+      }, 1500);
   })
 
 }
+
+function addToStock(){  
+  showTable();
+  console.log(`***** ${colors.green("Add to stock")} *****`);
+  connection.query("SELECT * FROM products", function(err, res){
+    inquirer
+    .prompt([
+      {
+        name: "item",
+        message: "Select item to add stock to: \n",
+        validate: function(val){
+          if(isNaN(val)){
+            return 'Please enter a number';
+          }else if(val > res.length){
+            return 'I am sorry, but you have entered an incorrect ID.'
+          }
+          else {
+            return true;
+          }
+        }
+      },
+      {
+        name: "amount",
+        message: "How much are you adding?",
+        validate: function(val){
+          if(isNaN(val)){
+            return 'Please enter a number';
+          }
+          else {
+            return true;
+          }
+        }
+      }
+    ])
+    .then((answers) => {
+      // console.log(typeof qty);//returns string
+      let qty = parseInt(answers.amount);
+      //item_id is one off of index bc of 0
+      let item = answers.item -1;
+      console.log(`You want to add ${qty} to ${answers.item}`);
+      
+      connection.query("UPDATE products SET ? WHERE ?;", [
+        {stock_quantity: res[item].stock_quantity + qty}, 
+        {item_id: answers.item} 
+      ])
+      console.log(`${colors.green("SUCCESS!")}`);
+      setTimeout(()=>{
+        console.log(`${res[item].product_name} is now ${(res[item].stock_quantity) + qty}`);
+        showTable();
+        setTimeout(()=>{
+          addMore();
+        }, 1000);
+        
+      }, 1000);
+    })
+  })
+}
+function addMore(){
+  inquirer
+    .prompt({
+      type: "confirm",
+      name: "addMore",
+      message: "Would you like to add more stock?"
+    })
+    .then((answer)=>{
+      if(answer.addMore){
+        addToStock();
+      }
+      else {
+        managerView();
+      }
+    })
+}
+// }
+// function addNewProduct(){
+//   console.log("Add new product fired");
+// }
+// function addToStock(){
+
+// }
 /*Manager View (Next Level)
 
 Create a new Node application called bamazonManager.js. Running this application will:
@@ -114,10 +175,10 @@ $$$$$ SWITCH: List a set of menu options:
 [✔]View Products for Sale viewProducts();
 the app should list every available item: the item IDs, names, prices, and quantities.
 
-[]View Low Inventory viewLowStock();
+[✔]View Low Inventory viewLowStock();
 then it should list all items with an inventory count lower than five.
 
-[]Add to Inventory addToStock();
+[✔]Add to Inventory addToStock();
 your app should display a prompt that will let the manager "add more" of any item currently in the store.
 
 []Add New Product addNewProduct();
@@ -137,4 +198,29 @@ If a manager selects Add New Product, it should allow the manager to add a compl
 
 ----*/
 
+//table fcn needed more than once
+function showTable(){
+  connection.query("SELECT * FROM products", function(err, res){
+    //instatiate the table:
+    var table = new Table({
+      head: ['ID', 'Item', 'Price', 'Qty'], 
+      colWidths: [5, 35, 15, 10]
+    });
+
+    if (err) throw err;
+
+    for (var i = 0; i < res.length; i++) {  
+      let qty = res[i].stock_quantity;
+      if(qty < 5){
+        qty = colors.magenta(qty)
+        }
+      table.push(
+        [res[i].item_id, res[i].product_name, res[i].price, qty]
+        )
+      };
+      console.log(table.toString());
+  })
+}
+
 managerView();
+
